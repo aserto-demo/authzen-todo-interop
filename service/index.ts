9588fca5f7ede9e2f280eb839930e9e4c8df88da
Omri Gazitt <ogazitt@gmail.com>
2024-03-10 22:57:56 -0700

    netlify fixes

--- service/index.ts
+++ service/index.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import cors from "cors";
-import { Store } from "./store";
-import { Server } from "./server";
-import { checkJwt, authzMiddleware } from "./auth";
+import { Store } from "./src/store";
+import { Server } from "./src/server";
+import { checkJwt, authzMiddleware } from "./src/auth";
 import * as dotenv from "dotenv";
 import * as dotenvExpand from "dotenv-expand";
+import { join, dirname } from "path";
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenvExpand.expand(dotenv.config());
 
+// establish whether we are hosted in Netlify
+const isNetlify = process.env.NETLIFY || process.env.REACT_APP_NETLIFY;
+
 const app: express.Application = express();
+const router = express.Router();
+const routerBasePath = isNetlify ? '/.netlify/functions/index' : '/';
+
 app.use(express.json());
 app.use(cors());
 
@@ -18,13 +29,25 @@ Store.open().then((store) => {
   const server = new Server(store);
   const checkAuthz = authzMiddleware(store);
 
-  app.get("/users/:userID", checkJwt, checkAuthz('can_read_user'), server.getUser.bind(server));
-  app.get("/todos", checkJwt, checkAuthz('can_read_todos'), server.list.bind(server));
-  app.post("/todos", checkJwt, checkAuthz('can_create_todo'), server.create.bind(server));
-  app.put("/todos/:id", checkJwt, checkAuthz('can_update_todo'), server.update.bind(server));
-  app.delete("/todos/:id", checkJwt, checkAuthz('can_delete_todo'), server.delete.bind(server));
+  router.get("/users/:userID", checkJwt, checkAuthz('can_read_user'), server.getUser.bind(server));
+  router.get("/todos", checkJwt, checkAuthz('can_read_todos'), server.list.bind(server));
+  router.post("/todos", checkJwt, checkAuthz('can_create_todo'), server.create.bind(server));
+  router.put("/todos/:id", checkJwt, checkAuthz('can_update_todo'), server.update.bind(server));
+  router.delete("/todos/:id", checkJwt, checkAuthz('can_delete_todo'), server.delete.bind(server));
 
-  app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-  });
+  // configure the router on the correct base path
+  app.use(routerBasePath, router);
+
+  // make it work with netlify functions
+  if (isNetlify) {
+    const serverless = require("serverless-http");
+    exports.handler = serverless(app);
+  } else {
+    // main endpoint serves react bundle from /build
+    app.use(express.static(join(__dirname, '..', 'build')));
+
+    app.listen(PORT, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    });
+  }
 });
